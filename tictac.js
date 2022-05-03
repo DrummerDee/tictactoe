b8b35605650215ca7cee9f2b932dd4db20e7640f
DrummerDee <Drummer.deja@gmail.com>
2022-05-03 01:19:36 -0400

    5/3

--- tictac.js
+++ tictac.js
@@ -1,29 +1,54 @@
-// create player one & two objects//
-function MakePlayer(playerName,playerMarker){
-    this.name = playerName
-    this.marker = playerMarker
-}
+let board;
+//let player1 = "O";
+//et player2 = "X";
+let currentPlayer = "X";
+let gameOver = false;
+//winner combos //
 
-let playerOne = new MakePlayer ('PlayerOne','x')
-let playerTwo = new MakePlayer ('PlayerTwo','o')
-
-const wins =[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+let winnerCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [6,3,6],
     [1,4,7],
     [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
-    [3,5,9],
-
+    [0,4,8],
+    [2,4,6],
 ]
-// on click for boxes//
 
-// loop needed//
-let i = 0;
-while ( i <= 9){
-    console.log(i);
-i++;
-} 
+let block = document.querySelectorAll('.block')
+// convert node list to array //
+block = Array.from(block)
+
+// click event for block //
+block.forEach(function(block){
+    block.addEventListener('click', function(){
+        if(block.innerText.trim() != "") return 
+        block.innerText= currentPlayer
+        youWin()
+        //code to where the player alternates//
+        currentPlayer = currentPlayer == "X" ? "O" : "X" 
+    })
+})
+
+//check for winner //
+function youWin(){
+    //loop for all winning combos//
+    winnerCombos.forEach(function(combo){
+        let check = combo.every(i => block[i].innerText.trim() == currentPlayer)
+        if(check){
+            aler(currentPlayer + ' is the winner')
+        }
+    }) 
+   }
+   // check to see if there is a tie //
+
+
+
+   //function for when I want to mark through the x and/or os//
+//function hightlightBlock(combo){
+    //combo.forEach(function(i){
+       // block(i).classList.add("highlight")
+    //}
+//}
+
