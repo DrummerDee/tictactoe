900d7d5e0aeac1a128af0ccbfab933e8b6e920ba
Drummer Dee <drummer.deja@gmail.com>
2022-07-06 15:38:49 -0400

    refactor

---
+++ .history/tictac_20220706151013.js
@@ -0,0 +1,77 @@
+const block = document.querySelectorAll('.block');
+const board = new Array(9).fill(null);
+const again = document.getElementById("play");
+let check;
+const O = "O";
+const X = "X";
+
+let currentPlayer = "X";
+
+
+// winner combinations //
+let winnerCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+// spaceClicked function //
+const spaceClicked = (e) => {
+    const id = e.target.id;
+
+ //prevent the characters from switching//
+if(!board[id]){ 
+    board[id] = currentPlayer;
+ e.target.innerText=currentPlayer;
+
+// you win function //
+if( youWin()){
+    return gameOver();
+}
+ currentPlayer = currentPlayer === X ? O : X;
+};
+}
+//smurf for clicked spaces//
+block.forEach((block) => block.addEventListener("click",spaceClicked));
+
+//play again smurf//
+again.addEventListener('click',playAgain);
+
+
+// function for youWin //
+function youWin(){
+    winnerCombos.forEach(function(combo){
+     check = combo.every(i => block[i].innerText == currentPlayer)
+     let test = true;
+     for(let i = 0 ; i < 9;i++){
+        if(block[i].innerText == ''){
+            test = false
+        }
+     }
+        
+        if(check){
+            document.getElementById('declare').innerText = (`${currentPlayer} is the winner`) 
+            
+             
+        }else if (test){
+            document.getElementById('declare').innerText = (`It's a tie`)     
+        }
+    })
+}
+
+
+
+// function for playAgain//
+function playAgain(){
+    document.getElementById('declare').innerText = ''
+    currentPlayer = X;
+    board.fill(null);
+    block.forEach((block)=>{
+        block.innerHTML = ""
+    })
+}
+
